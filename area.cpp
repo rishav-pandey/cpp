12b917649c8d76b25a9b79ff19b22cf3af7acd2f
Rishav <pandeyrishav2@gmail.com>
2022-03-05 00:43:22 +0530

    updated

--- area.cpp
+++ area.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main()
 {
-    int b,h,area;
+    float b,h,area;
     cout<<"Enter base and height"<<endl;
     cin>>b>>h;
-    area=
+    area=(b*h);
+    cout<<"area"<<area;
 }
