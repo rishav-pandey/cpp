999491b00379cb4bff92d41e6be7659fe5987ff7
Rishav <pandeyrishav2@gmail.com>
2022-03-05 00:53:11 +0530

    Create sum_natural_no.cpp

---
+++ sum_natural_no.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Enter N"<<endl;
+    int n,sum;
+    cin>>n;
+    sum=(n*(n+2))/2;
+    cout<<"sum of natural no is "<<sum;
+    return 0;
+}
