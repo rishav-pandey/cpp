4ea29667236f6e0a098f0649dbb18841d846cd0b
Rishav <pandeyrishav2@gmail.com>
2022-03-05 01:17:30 +0530

    Update quadratic.cpp

--- quadratic.cpp
+++ quadratic.cpp
@@ -3,15 +3,16 @@
 using namespace std;
 int main()
 {    
-    float a,b,c,x;
+    float a,b,c,x1,x2;
     cout<<"Enter coefficient of x square"<<endl;
     cin>>a;
     cout<<"Enter coefficient of x"<<endl;
     cin>>b;
     cout<<"Enter constant"<<endl;
     cin>>c;
-    x=(-b+sqrt(b*b-4*a*c))/(2*a);
-    cout<<"value is "<<x;
+    x1=(-b+sqrt(b*b-4*a*c))/(2*a);
+    x2=(-b-sqrt(b*b-4*a*c))/(2*a);
+    cout<<"value is "<<x1<<" "<<x2;
 
     return 0;
 }
