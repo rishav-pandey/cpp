ed5156fd3d03ad75004cfbd06413738c7173d0ad
Rishav <pandeyrishav2@gmail.com>
2022-03-06 23:31:32 +0530

    Create oddeven.cpp

---
+++ oddeven.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int a;
+   cout<<"Enter no"<<endl;
+   cin>>a;
+   if(a%2==0)
+   cout<<"The no is even"<<endl;
+   else
+   cout<<"The no is odd"<<endl;
+return 0;
+}
