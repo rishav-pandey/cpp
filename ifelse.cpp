d9f41fc2527456ee60f8c7c725e7154da1671cb6
Rishav <pandeyrishav2@gmail.com>
2022-03-06 23:53:38 +0530

    Create ifelse.cpp

---
+++ ifelse.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int day;
+        cout<<"enter the day number";
+        cin>>day;
+        if(day==1)
+           cout<<"monday"<<endl;
+        else if(day==2)
+             cout<<"tuesday"<<endl;
+        else if(day==3)
+             cout<<"wednesday"<<endl;
+        else if(day==4)
+             cout<<"thursday"<<endl;
+        else if(day==5)
+	     cout<<"friday"<<endl;
+        else if(day==6)
+             cout<<"saturday"<<endl;
+        else if(day==7)
+             cout<<"sunday"<<endl;
+        else
+             cout<<"invalid day no"<<endl;
+return 0;
+}
