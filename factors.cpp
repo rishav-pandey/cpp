a7a49cdc037097f8470f72f9aa2d8a3f594984c1
Rishav <pandeyrishav2@gmail.com>
2022-03-08 02:03:47 +0530

    Create factors.cpp

---
+++ factors.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int n,i,div;
+   cout<<"Enter the no :"<<endl;
+   cin>>n;
+   cout<<"Factors are:"<<endl;
+   for(i=1;i<=n;i++)
+   {
+       if(n%i==0)
+       cout<<i<<endl;
+   }
+    return 0;
+}
